f104c0466e72e8122ddb13709e24a89b63763121
jehan <jehanke8@gmail.com>
2024-03-26 22:37:22 +0530

    fixed incorrect animation

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -84,8 +84,8 @@ const HomePage = () => {
     <>
       <motion.div 
         className={isOpaque ? "opaque-home-container" : "home-container"}
-        initial={{ y: -20 }}
-        animate={{ y: 0 }}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
         transition={{duration: 0.7,ease: [0.6, -0.05, 0.01, 0.99]}}
       >
         <div className="home-header">
